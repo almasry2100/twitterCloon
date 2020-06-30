dab588e6c10243f420b0c403941267f8f192fad7
elsay <almasry2100@yahoo.com>
2020-07-01 03:01:44 +0400

    ini Var for sign up activity

--- app/src/main/java/com/example/ac_twittercloone/MainActivity.java
+++ app/src/main/java/com/example/ac_twittercloone/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ac_twittercloone;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmailId,edtSignUpUserName,edtSignUpPassWord;
+    private Button btnSignUp,btnLogin;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
