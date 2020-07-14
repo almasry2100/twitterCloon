b1d699f1c3c29d67851b703deb9066d2b3ffa57f
elsay <almasry2100@yahoo.com>
2020-07-15 01:10:00 +0400

    HashMap

--- app/src/main/java/com/example/ac_twittercloone/LoginActivity.java
+++ app/src/main/java/com/example/ac_twittercloone/LoginActivity.java
@@ -1,14 +1,69 @@
 package com.example.ac_twittercloone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtLoginID,edtLoginPassWord;
+    private Button btnLogin,btnLoginSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+        edtLoginID =findViewById( R.id.edtLoginID );
+        edtLoginPassWord=findViewById( R.id.edtLoginPassWord );
+        btnLogin=findViewById( R.id.btnLogin );
+        btnLoginSignUp=findViewById( R.id.btnLoginSignUp );
+
+        btnLogin.setOnClickListener(this  );
+        btnLoginSignUp.setOnClickListener( this );
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                if (edtLoginID.getText().toString().equals( "" )|| edtLoginPassWord.getText().toString().equals( "" )){
+
+                    Toast.makeText( this,"user name of password missing  ",Toast.LENGTH_SHORT ).show();
+
+                }else {
+                    ParseUser.logInInBackground( edtLoginID.getText().toString(), edtLoginPassWord.getText().toString(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if (user !=null & e==null){
+                                FancyToast.makeText( LoginActivity.this,user.getUsername()+"  login ", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true ).show();
+                                Intent intent =new Intent( LoginActivity.this,TwitterUsers.class );
+                                startActivity( intent );
+
+                            }else {
+                                Toast.makeText( LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG ).show();
+
+                            }
+
+                        }
+                    } );
+
+
+                }
+
+        }
     }
 }
--- app/src/main/java/com/example/ac_twittercloone/MainActivity.java
+++ app/src/main/java/com/example/ac_twittercloone/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.ac_twittercloone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edtEmailId,edtSignUpUserName,edtSignUpPassWord;
     private Button btnSignUp,btnLogin;
     
@@ -20,14 +26,53 @@ public class MainActivity extends AppCompatActivity {
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
 
-        edtEmailId.findViewById( R.id.edtEmailId );
-        edtSignUpUserName.findViewById( R.id.edtSignUpUserName );
-        edtSignUpPassWord.findViewById( R.id.edtSignUpPassWord );
+        edtEmailId=findViewById( R.id.edtEmailId );
+        edtSignUpUserName=findViewById( R.id.edtSignUpUserName );
+        edtSignUpPassWord=findViewById( R.id.edtSignUpPassWord );
 
-        btnSignUp.findViewById( R.id.btnSignUp );
-        btnLogin.findViewById( R.id.btnLogin );
+        btnSignUp=findViewById( R.id.btnSignUp );
+        btnLogin=findViewById( R.id.btnLogin );
+
+        btnSignUp.setOnClickListener( this );
+
+        btnLogin.setOnClickListener( this );
 
         
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnSignUp:
+                ParseUser userApp = new ParseUser();
+                userApp.setEmail( edtEmailId.getText().toString() );
+                userApp.setUsername( edtSignUpUserName.getText().toString() );
+                userApp.setPassword( edtSignUpPassWord.getText().toString() );
+                userApp.signUpInBackground( new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null){
+                            FancyToast.makeText(MainActivity.this," is Signed Up",FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
+                            Intent intent=new Intent( MainActivity.this,TwitterUsers.class );
+                            startActivity( intent );
+                        }else {
+                            FancyToast.makeText(MainActivity.this," error",FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
+
+                        }
+                    }
+                } );
+
+                break;
+            case R.id.btnLogin:
+                Intent intent =new Intent( MainActivity.this,LoginActivity.class );
+                startActivity( intent );
+
+
+
+
+                break;
+        }
 
     }
 }
